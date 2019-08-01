2f77d95445903543f4fc8bf419714a0bf8190bb7
vteso <vtesoho@163.com>
2019-08-01 21:53:47 +0800

    appbar

--- flutter_style/lib/page/style/style.dart
+++ flutter_style/lib/page/style/style.dart
@@ -60,26 +60,124 @@ class AppBarWidget extends StatefulWidget {
 class _AppBarWidgetState extends State<AppBarWidget> {
   double elevation = 0.0;
   bool leading = false;
+  String title = 'AppBar';
+  List<Widget> actions;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // leading: leading ? Container() : null,
+        // leading: ,
         automaticallyImplyLeading: leading, //去除默认返回按钮 
-        title: Text('AppBar'),
+        title: Text('${title}'),
         elevation: elevation,  //阴影
-
+        actions: actions,
       ),
       body: Column(
         children: <Widget>[
           elevationChange(),
           leadingChange(),
+          titleChange(),
+          actionsChange(),
         ],
       ),
     );
   }
 
+  Widget actionsChange(){
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Text('右边按纽区域'),
+        ),
+        Expanded(
+          flex: 3,
+          child: Row(
+            children: <Widget>[
+              PopupMenuButton(
+                itemBuilder:(context ){
+                  
+                }
+              ),
+              Radio(
+                value: actionChangeItemMore(),
+                groupValue: 'actions',
+                onChanged: (val){
+                  actions = val;
+                  print('${val}');
+                  setState(() {
+                    
+                  });
+                },
+              ),
+              Text('普通按纽'),
+              SizedBox(
+                width: 50,
+              ),
+              Radio(
+                value: actionChangeItemOne(),
+                groupValue: 'actions',
+                onChanged: (val){
+                  print('${val}');
+                  actions = val;
+                  setState(() {
+                    
+                  });
+                },
+              ),
+              Text('二级按纽'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 
+  List<Widget> actionChangeItemOne(){
+    List<Widget> data = [];
+    data.add(Icon(Icons.edit),);
+    return data;
+  }
+
+  List<Widget> actionChangeItemMore(){
+    List<Widget> data = [];
+    data.add(Icon(Icons.edit),);
+    data.add(Icon(Icons.map),);
+    data.add(Icon(Icons.email),);
+    return data;
+  }
+
+  Widget titleChange(){
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Text('标题'),
+        ),
+        Expanded(
+          flex: 3,
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 150,
+                child: TextField(
+                  onChanged: (val){
+                    setState(() {
+                      title = val;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Text(''),
+        )
+      ],
+    );
+  }
 
   Widget leadingChange(){
     return Row(
