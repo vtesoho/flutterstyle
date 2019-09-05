9730cbe1233e4906b2f51d93aef9009bc345e9d8
vteso <vtesoho@163.com>
2019-09-05 23:35:55 +0800

    test http2

--- flutter_style/lib/page/demo/http2/http2_demo.dart
+++ flutter_style/lib/page/demo/http2/http2_demo.dart
@@ -10,8 +10,9 @@ class HttpDemo extends StatefulWidget {
   _HttpDemoState createState() => _HttpDemoState();
 }
 
+
 class _HttpDemoState extends State<HttpDemo> {
-  
+  ClientTransportConnection transport;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +27,23 @@ class _HttpDemoState extends State<HttpDemo> {
                   testHttp();
                 },
                 child: Container(
-                  child: Text('测试http2'),
+                  child: Text('连接http2'),
+                ),
+              ),
+              GestureDetector(
+                onTap: (){
+                  finishHttp();
+                },
+                child: Container(
+                  child: Text('关闭http2'),
+                ),
+              ),
+              GestureDetector(
+                onTap: (){
+                  showHttp();
+                },
+                child: Container(
+                  child: Text('打印http2'),
                 ),
               ),
            ],
@@ -35,22 +52,34 @@ class _HttpDemoState extends State<HttpDemo> {
     );
   }
 
+  showHttp() async {
+    print('----------transport.isOpen ${transport.isOpen}');
+  }
+
+  finishHttp() async{
+    await transport.finish();
+  }
+
   testHttp() async{
     var uri = Uri.parse('https://www.shuachi.com/cate/newfood5O68.html');
     
-    var transport = new ClientTransportConnection.viaSocket(
+    transport = new ClientTransportConnection.viaSocket(
       await SecureSocket.connect(
         uri.host,
         uri.port,
         supportedProtocols: ['h2'],
       ),
     );
+
+    
+
+    
     
     transport.ping().then((onValue){
       print('------------onvalue $onValue');
     });
-    print('-------------url $transport ping');
-
+    print('-------------url $transport ping ${uri.scheme}');
+    
     var stream = transport.makeRequest(
       [
         new Header.ascii(':method', 'GET'),
@@ -60,6 +89,7 @@ class _HttpDemoState extends State<HttpDemo> {
       ],
       endStream: true,
     );
+    
 
     await for (var message in stream.incomingMessages) {
       if (message is HeadersStreamMessage) {
@@ -72,8 +102,12 @@ class _HttpDemoState extends State<HttpDemo> {
         // Use [message.bytes] (but respect 'content-encoding' header)
       }
     }
-    await transport.finish();
+
+    // await transport.finish();
   }
+
+
+
 
 
 }
