dc4fbe066787d030d513ba350b8b9f548bb66069
vteso <vtesoho@163.com>
2019-11-28 20:16:46 +0800

    testcliprect

---
+++ flutter_style/lib/page/demo/circularReveal/text_clipRect.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TestClipRect extends StatelessWidget {
+  const TestClipRect({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'text'
+        ),
+      ),
+    );
+  }
+}
