1d0bbe4e4bf2054c549beb7c2ff373064d8a5fd7
vteso <vtesoho@163.com>
2019-12-16 23:34:37 +0800

    transform

---
+++ flutter_style/lib/page/transform/transform.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TransformDemo extends StatefulWidget {
+  TransformDemo({Key key}) : super(key: key);
+
+  @override
+  _TransformDemoState createState() => _TransformDemoState();
+}
+
+class _TransformDemoState extends State<TransformDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text('data'),
+    );
+  }
+}
