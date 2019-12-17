f28e5247f020756854a95f47b06cfb0805b99fa4
vteso <vtesoho@163.com>
2019-12-17 20:55:36 +0800

    transform

--- flutter_style/lib/page/transform/transform.dart
+++ flutter_style/lib/page/transform/transform.dart
@@ -10,8 +10,41 @@ class TransformDemo extends StatefulWidget {
 class _TransformDemoState extends State<TransformDemo> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('data'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('transform'),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            alignmentAction(),
+          ],
+        ),
+      ),
+      body: Container(
+        child: Transform(
+          alignment: alignment,
+          transform: transform,
+          
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            color: Colors.deepOrange,
+            child: const Text('Apartment for rent!'),
+          ),
+        ),
+      ),
     );
   }
+
+  Matrix4 transform = Matrix4.skewX(0.3);
+  transformAction(){
+    
+  }
+
+  Alignment alignment = Alignment.center;
+  alignmentAction(){
+
+  }
+
+
 }
